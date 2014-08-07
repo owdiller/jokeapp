056f387dd1a43de6af33b4c48ab605ff3a8f71aa
Your Name <you@example.com>
2014-08-07 21:08:24 +0000

    final final v1.2.0

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,11 +2,5 @@ class ApplicationController < ActionController::Base
     
  def login_required
  end
- #   username = User.find_by(id: session['user_id'])
-#    if username.blank?
-   #    redirect_to('/home')
-  #   end
-
- # end
-  #    protect_from_forgery with: :exception
+    protect_from_forgery with: :exception
 end
